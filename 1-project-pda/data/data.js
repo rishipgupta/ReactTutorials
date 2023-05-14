ca6b9dca9d8008968b546de431c680a14260b4f0
Riship Gupta <guptariship@gmail.com>
2023-05-14 17:10:54 +0530

    1-project-pda created, components and data set up

---
+++ 1-project-pda/data/data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 1,
+    imageSrc: "../images/alexa.png",
+    title: "Alexa",
+    twitterProfile: "@alexa99",
+    description: "I'll help you buy stuff off Amazon"
+  },
+  {
+    id: 2,
+    imageSrc: "../images/cortana.png",
+    title: "Cortana",
+    twitterProfile: "@cortana12",
+    description: "Personal assistant by Microsoft"
+  },
+  {
+    id: 1,
+    imageSrc: "../images/siri.png",
+    title: "Siri",
+    twitterProfile: "@siri44",
+    description: "I don't get a lot of updates anymore"
+  }
+];
+
+export default data;

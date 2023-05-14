7671abcf5ca0ef42a4f1055eed68483f6d708256
Riship Gupta <guptariship@gmail.com>
2023-05-14 21:12:15 +0530

    Moved image imports to ProfileCard component

--- src/1-project-pda/components/ProfileCard.js
+++ src/1-project-pda/components/ProfileCard.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import '../styles/profilecard.css'
 
+// For showing images in our local machine we need to import them specifically
+// For external images direct links would work.
+
+import AlexaImage from '../images/alexa.png';
+import CortanaImage from '../images/cortana.png';
+import SiriImage from '../images/siri.png';
+
+let imgArr = [AlexaImage, CortanaImage, SiriImage]
 
 const ProfileCard = (props) => {
-  const { src, title, twitterProfile, description } = props;
+  const { id, title, twitterProfile, description } = props;
   return (
     <div className='container' >
       <div className='image-container'>
-        <img src={src} alt="PDA" className='pda-image' />
+        <img src={imgArr[id - 1]} alt="PDA" className='pda-image' />
       </div>
       <div>
         <h3>{title}</h3>
--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import ProfileCard from './1-project-pda/components/ProfileCard';
 import data from './1-project-pda/data/data';
-import AlexaImage from './1-project-pda/images/alexa.png'
-import CortanaImage from './1-project-pda/images/cortana.png'
-import SiriImage from './1-project-pda/images/siri.png'
+
 
 function App() {
-  // For showing images in our local machine we need to import them specifically
-  // For external images direct links would work.
+
   return (
     <>
       <div style={{ backgroundColor: "lightgreen", padding: "10px" }}>Personal Digital Assistants</div>
       {data.map((card) => {
-        let imageSrc = "";
-        if (card.id === 1) {
-          imageSrc = AlexaImage
-        } else if (card.id === 2) {
-          imageSrc = CortanaImage
-        }
-        else if (card.id === 3) {
-          imageSrc = SiriImage;
-        }
-        return <ProfileCard src={imageSrc} title={card.title} twitterProfile={card.twitterProfile} description={card.description} />
+        return <ProfileCard id={card.id} title={card.title} twitterProfile={card.twitterProfile} description={card.description} />
       })}
     </>
   )
